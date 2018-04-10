3398c3d7a1e11afeee69c220035779f7fe7b1c31
markhorvath <markhorvath7@gmail.com>
2018-04-10 16:49:34 -0400

    lec117 outputting burger ingredients dynamically, notes around transformedIngredients double map

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,37 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    // since state ingredients are an object, must find a way to transform
+    // them into an array of igredients.  Object Object is a default javascript
+    // object that has a keys method which extracts the keys of a given object
+    // and turns them into an array
+    // const test = Object.keys(props.ingredients);
+    // console.log(test);
+    // const test2 = test.map(igKey => {
+    //     return [...Array(props.ingredients[igKey])];
+    // })
+    // console.log(test2);
+    // const test3 = test.map(ingKey => {
+    //         return [...Array(props.ingredients[ingKey])].map((_, i) => {
+    //             return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+    //         });
+    //     });
+    // console.log(test3);
+
+    // _ as an argument in the second .map() call is just a blank
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingKey => {
+            return [...Array(props.ingredients[ingKey])].map((_, i) => {
+                // console.log([...Array(props.ingredients[ingKey])]);
+                // console.log(ingKey);
+                // console.log("value of i: " + i);
+                return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+            });
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+                {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
