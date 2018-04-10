5c85b3bc1e176e3c2c0778812d679e21c8070410
markhorvath <markhorvath7@gmail.com>
2018-04-10 09:32:43 -0400

    lec113 added burgerbuilder container, some css to layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
 
 const layout = (props) => (
     // Could/should I use <Fragment> here instead of Aux?
     <Aux>
         <div>Toolbar, Sidedrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
