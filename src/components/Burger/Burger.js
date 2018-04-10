794cba229843ee750a7b231c7a3772cf1c900753
markhorvath <markhorvath7@gmail.com>
2018-04-10 17:29:07 -0400

    lec118 calculating ingredient sum dynamically using reduce function to turn transformedingredients in a single array of jsx objects

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -21,7 +21,7 @@ const burger = (props) => {
     // console.log(test3);
 
     // _ as an argument in the second .map() call is just a blank
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingKey => {
             return [...Array(props.ingredients[ingKey])].map((_, i) => {
                 // console.log([...Array(props.ingredients[ingKey])]);
@@ -29,7 +29,19 @@ const burger = (props) => {
                 // console.log("value of i: " + i);
                 return <BurgerIngredient key={ingKey + i} type={ingKey} />;
             });
-        });
+        })
+        // reduce() is a built-in array function to transform an array into something else,
+        // it takes a function as an input this function receives two arguments passed in
+        // automatically by javascript, the previous value (arr) and the current value (el)
+
+        // it also accepts an initial value, the empty array []
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+        // console.log(transformedIngredients);
+        if (transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please start adding ingredients</p>
+        }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,8 +6,8 @@ class BurgerBuilder extends Component {
     state = {
         ingredients: {
             salad: 1,
-            bacon: 1,
-            cheese: 2,
+            bacon: 2,
+            cheese: 3,
             meat: 2
         }
     }
