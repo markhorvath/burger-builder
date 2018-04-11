cdf0b17ebd175f5dc8d94cbad60a79b20aa64a1a
markhorvath <markhorvath7@gmail.com>
2018-04-11 10:04:12 -0400

    lec120 outputting buildcontrols, fixed some return statements where i used {} instead of ()

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import classes from './BuildControl.css';
 
-const buildControl = (props) => {
+const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
         <button className={classes.Less}>Remove</button>
         <button className={classes.More}>Add</button>
     </div>
-};
+);
 
-export default buildControls;
+export default buildControl;
