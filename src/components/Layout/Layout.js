59cf86758d34bd00e2dccdfbc8d974199e7f0bb7
markhorvath <markhorvath7@gmail.com>
2018-04-12 18:31:34 -0400

    creating a responsive sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     // Could/should I use <Fragment> here instead of Aux?
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
