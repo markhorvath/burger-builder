d0e2a7c1587a302d8ea948e759747b39e3ada0c5
markhorvath <markhorvath7@gmail.com>
2018-04-12 14:29:47 -0400

    lec131 added toolbar component and css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     // Could/should I use <Fragment> here instead of Aux?
     <Aux>
-        <div>Toolbar, Sidedrawer, Backdrop</div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
