83cd41bbb7abc05c410dcaafc5ac59c6f1fbfb51
markhorvath <markhorvath7@gmail.com>
2018-04-13 10:01:22 -0400

    lec135 working on responsive adjustments to sidedrawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,12 @@ import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 
+// Setting height in Logo as props can then be used to style the Logo component
+// in-line which will override the Logo.css setting
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo />
+        <Logo height="80%" />
         <nav>
             <NavigationItems />
         </nav>
