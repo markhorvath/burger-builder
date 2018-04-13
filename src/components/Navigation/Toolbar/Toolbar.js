61549c58e9c9aa821cc6c4ed63f144133ca36833
markhorvath <markhorvath7@gmail.com>
2018-04-13 10:38:41 -0400

    lec136 hide toolbar on mobile, small aesthetic changes to sidedrawer, set relevent css files to code for mobile first

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,12 +4,12 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 
 // Setting height in Logo as props can then be used to style the Logo component
-// in-line which will override the Logo.css setting
+// in-line which will override the Logo.css setting, also done in Sidedrawer
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
         <Logo height="80%" />
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
