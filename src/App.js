690efd11e4c888534bfeed6644c113882a2ed305
markhorvath <markhorvath7@gmail.com>
2018-04-23 15:39:29 -0400

    lec208 set up routes for burgerbuilder and checkout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-
 
 class App extends Component {
 
@@ -10,8 +10,10 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
