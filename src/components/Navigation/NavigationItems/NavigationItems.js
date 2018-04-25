7ed5901702c30556611f0deba857def82d44c0b5
markhorvath <markhorvath7@gmail.com>
2018-04-25 11:24:46 -0400

    lec215 implementing navigation lins

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,9 +3,14 @@ import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) => (
+
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" active>BurgerBuiler</NavigationItem>
-        <NavigationItem link="/">Checkout</NavigationItem>
+{/*    i f you only need active to be on one particular link, you could pass
+    the exact property to NavigationItem here in example below, then in NavigationItem.js
+    set the exact property to exact={props.exact}
+    {/*<NavigationItem link="/" exact>Example for exact/active</NavigationItem>*/}
+        <NavigationItem link="/" exact>BurgerBuilder</NavigationItem>
+        <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
 )
 
