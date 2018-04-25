423e18536a7163cc0c69f4cf2c1763d67b19160f
markhorvath <markhorvath7@gmail.com>
2018-04-25 17:52:33 -0400

    lec216 fetching orders

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,7 +26,6 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
-        console.log(this.props);
         axios.get('https://react-burger-12b1a.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ingredients: response.data})
